8a346ab5fe6eda071369f21696d06de0ae0ae4b1
Coda Hale <coda.hale@gmail.com>
2014-06-26 10:48:14 -0700

    Added latency tracking for requests.

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -1,17 +1,23 @@
-// Package metrics provides an HTTP handler which registers atomic expvars for
-// the number of requests received and responses sent.
+// Package metrics provides an HTTP handler which registers expvar counters for
+// the number of requests received and responses sent as well as quantiles of
+// the latency of responses.
 package metrics
 
 import (
 	"expvar"
 	"net/http"
+	"sync"
 	"sync/atomic"
+	"time"
+
+	"github.com/bmizerany/perks/quantile"
 )
 
 // Wrap returns a handler which records the number of requests received and
-// responses sent to the given handler.
+// responses sent to the given handler, as well as latency quantiles for
+// responses which are reset every minute.
 //
-// These counters are published as http.Requests and http.Responses in expvars.
+// These counters are published as the "http" object in expvars.
 //
 // By tracking incoming requests and outgoing responses, one can monitor not
 // only the requests per second, but also the number of requests being processed
@@ -21,19 +27,62 @@ func Wrap(h http.Handler) http.Handler {
 		atomic.AddUint64(&requests, 1)
 		defer atomic.AddUint64(&responses, 1)
 
+		start := time.Now()
 		h.ServeHTTP(w, r)
+		elapsed := time.Now().Sub(start).Seconds() * 1000.0
+
+		latencyMutex.Lock()
+		defer latencyMutex.Unlock()
+
+		latency.Insert(elapsed)
 	})
 }
 
 var (
 	requests, responses uint64
+	latency             *quantile.Stream
+	latencyMutex        sync.Mutex
 )
 
 func init() {
+	latency = quantile.NewTargeted(0.50, 0.75, 0.90, 0.95, 0.99, 0.999)
+
+	go func() {
+		reset := time.NewTicker(1 * time.Minute)
+		for _ = range reset.C {
+			latencyMutex.Lock()
+			defer latencyMutex.Unlock()
+
+			latency.Reset()
+		}
+	}()
+
 	expvar.Publish("http", expvar.Func(func() interface{} {
-		return map[string]uint64{
-			"Requests":  atomic.LoadUint64(&requests),
-			"Responses": atomic.LoadUint64(&responses),
+		req, res := atomic.LoadUint64(&requests), atomic.LoadUint64(&responses)
+
+		latencyMutex.Lock()
+		defer latencyMutex.Unlock()
+
+		return httpStats{
+			Requests:  req,
+			Responses: res,
+			Latency: latencyStats{
+				P50:  latency.Query(0.50),
+				P75:  latency.Query(0.75),
+				P90:  latency.Query(0.90),
+				P95:  latency.Query(0.95),
+				P99:  latency.Query(0.99),
+				P999: latency.Query(0.999),
+			},
 		}
 	}))
+}
+
+type httpStats struct {
+	Requests, Responses uint64
+	Latency             latencyStats
+}
+
+type latencyStats struct {
+	P50, P75, P90, P95, P99, P999 float64
 }
