dbff336054f97b82252ff8487230779f060c142f
Coda Hale <coda.hale@gmail.com>
2014-06-27 11:32:32 -0700

    Dump stacktrace on SIGUSR1.

--- service/service.go
+++ service/service.go
@@ -1,10 +1,17 @@
 // Package service combines the other various packages in http-handlers to
-// provide an operations-friendly http.Handler for your application.
+// provide an operations-friendly http.Handler for your application. Including
+// this package will also allow you to dump a full stack trace to stderr by
+// sending your application the SIGUSR1 signal.
 package service
 
 import (
+	"fmt"
 	"net/http"
 	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+	"time"
 
 	"github.com/codahale/http-handlers/debug"
 	"github.com/codahale/http-handlers/logging"
@@ -27,4 +34,16 @@ func Wrap(h http.Handler) http.Handler {
 		),
 		os.Stdout,
 	)
+}
+
+func init() {
+	dump := make(chan os.Signal)
+	go func() {
+		stack := make([]byte, 16*1024)
+		for _ = range dump {
+			n := runtime.Stack(stack, true)
+			fmt.Fprintf(os.Stderr, "==== %s\n%s\n====\n", time.Now(), stack[0:n])
+		}
+	}()
+	signal.Notify(dump, syscall.SIGUSR1)
 }
