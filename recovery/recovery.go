907a8f825ee37c11a1821ee1d6226b456ce1d5aa
Coda Hale <coda.hale@gmail.com>
2014-08-31 15:41:54 -0700

    Make panic handlers responsible for concurrency.

--- recovery/recovery.go
+++ recovery/recovery.go
@@ -20,11 +20,16 @@ type PanicHandler func(id int64, err interface{}, stacktrace []string)
 // LogOnPanic logs the given panic and its stacktrace, prefixing each line with
 // the panic ID.
 func LogOnPanic(id int64, err interface{}, stacktrace []string) {
+	logMutex.Lock()
+	defer logMutex.Unlock()
+
 	log.Printf("panic=%016x message=%v\n", id, err)
 	for _, line := range stacktrace {
 		log.Printf("panic=%016x %s", id, line)
 	}
 }
+
+var logMutex sync.Mutex
 
 // Wrap returns an handler which proxies requests to the given handler, but
 // handles panics by logging the stack trace and returning a 500 Internal Server
@@ -43,13 +48,9 @@ func (h *recoveryHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	defer func() {
 		e := recover()
 		if e != nil {
-
-			h.r.Lock()
-			defer h.r.Unlock()
 			panics.Add()
 
 			id := rand.Int63()
-
 			var lines []string
 			for skip := 1; ; skip++ {
 				pc, file, line, ok := runtime.Caller(skip)
@@ -63,7 +64,6 @@ func (h *recoveryHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 				s := fmt.Sprintf("%s:%d %s()\n", file, line, f.Name())
 				lines = append(lines, s)
 			}
-
 			h.p(id, e, lines)
 
 			body := fmt.Sprintf(
